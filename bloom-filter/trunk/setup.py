1efd1ab1473d1f1475e510a3844e61084d3956f1
dstromberg <dstromberg@69029ff4-fcd3-493e-9d25-be61515a9e32>
2012-11-07 17:42:31 +0000

    Version bump

--- bloom-filter/trunk/setup.py
+++ bloom-filter/trunk/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "drs-bloom-filter",
-    version = "1.0",
+    version = "1.01",
     packages = find_packages(),
     scripts = ['bloom_filter_mod.py', 'python2x3.py' ],
 
