6a3d9fa518943684a12bdd62c27a843637132243
Harshad Sharma <harshad@sharma.io>
2017-04-30 15:40:22 +0530

    Cleanup setup.

--- setup.py
+++ setup.py
@@ -3,23 +3,12 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-import io
-import re
 from glob import glob
 from os.path import basename
-from os.path import dirname
-from os.path import join
 from os.path import splitext
 
 from setuptools import find_packages
 from setuptools import setup
-
-
-def read(*names, **kwargs):
-    return io.open(
-        join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
-    ).read()
 
 
 setup(
@@ -28,7 +17,6 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-    # scripts=['drs_bloom_filter.py', 'python2x3.py'],
 
     # metadata for upload to PyPI
     author="Daniel Richard Stromberg",
