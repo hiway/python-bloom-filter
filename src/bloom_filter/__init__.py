77ec72a937bfdafa961ba04db31e391280b3b2ff
Harshad Sharma <harshad@sharma.io>
2017-04-30 17:04:16 +0530

    Expose method needed by test.

--- src/bloom_filter/__init__.py
+++ src/bloom_filter/__init__.py
@@ -3,10 +3,12 @@
 
 from .bloom_filter import (
     BloomFilter,
+    get_bitno_lin_comb,
     get_bitno_seed_rnd,
 )
 
 __all__ = [
     'BloomFilter',
+    'get_bitno_lin_comb',
     'get_bitno_seed_rnd',
 ]
